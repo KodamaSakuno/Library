0b43d16b0152b2f11fafb691732f724a5ccc4249
神樹桜乃 <kodama@sakuno.moe>
2015-08-18 23:12:56 +0800

    + High dpi support

---
+++ Sakuno.UserInterface/Dpi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sakuno.UserInterface
+{
+    public struct Dpi : IEquatable<Dpi>
+    {
+        public static Dpi Default { get; } = new Dpi(96, 96);
+
+        public uint X { get; }
+        public uint Y { get; }
+
+        public double ScaleX => X / (double)Default.X;
+        public double ScaleY => Y / (double)Default.Y;
+
+        public Dpi(uint x, uint y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public bool Equals(Dpi rpOther) => X == rpOther.X && Y == rpOther.Y;
+        public override bool Equals(object rpObject) => rpObject != null ? Equals((Dpi)rpObject) : false;
+        public override int GetHashCode() => ((int)X * 401) ^ (int)Y;
+
+        public static bool operator ==(Dpi x, Dpi y) => x.Equals(y);
+        public static bool operator !=(Dpi x, Dpi y) => !x.Equals(y);
+
+        public override string ToString() => $"{X}, {Y} ({ScaleX}, {ScaleY})";
+    }
+}
---
+++ Sakuno.UserInterface/VisualExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Sakuno.UserInterface
+{
+    public static class VisualExtensions
+    {
+        public static Dpi? GetSystemDpi(this Visual rpVisual)
+        {
+            var rSource = PresentationSource.FromVisual(rpVisual);
+            return new Dpi((uint)(Dpi.Default.X * rSource?.CompositionTarget?.TransformToDevice.M11), (uint)(Dpi.Default.Y * rSource?.CompositionTarget?.TransformToDevice.M22));
+        }
+    }
+}
