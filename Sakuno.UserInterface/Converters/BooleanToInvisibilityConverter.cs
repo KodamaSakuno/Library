87066d9f20153267f18cce466075d39762a1c5f2
神樹桜乃 <kodama@sakuno.moe>
2015-10-30 17:33:29 +0800

    Boolean to invisibility converter

---
+++ Sakuno.UserInterface/Converters/BooleanToInvisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Sakuno.UserInterface.Converters
+{
+    public class BooleanToInvisibilityConverter : IValueConverter
+    {
+        public object Convert(object rpValue, Type rpTargetType, object rpParameter, CultureInfo rpCulture)
+        {
+            var rBoolean = false;
+
+            if (rpValue is bool)
+                rBoolean = (bool)rpValue;
+            else if (rpValue is bool?)
+            {
+                var rNullableBoolean = (bool?)rpValue;
+                rBoolean = rNullableBoolean.HasValue && rNullableBoolean.Value;
+            }
+
+            return rBoolean ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object rpValue, Type rpTargetType, object rpParameter, CultureInfo rpCulture) =>
+            rpValue is Visibility ? (Visibility)rpValue == Visibility.Collapsed : false;
+    }
+}
