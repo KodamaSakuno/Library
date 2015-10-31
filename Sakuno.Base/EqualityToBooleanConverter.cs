115efe6a4fb7dc770357cdbb715a0740f563c969
神樹桜乃 <kodama@sakuno.moe>
2015-10-31 09:27:52 +0800

    Equality to boolean converter

---
+++ Sakuno.Base/EqualityToBooleanConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Sakuno
+{
+    public class EqualityToBooleanConverter : IValueConverter
+    {
+        public object Convert(object rpValue, Type rpTargetType, object rpParameter, CultureInfo rpCulture) => rpValue.Equals(rpParameter);
+
+        public object ConvertBack(object rpValue, Type rpTargetType, object rpParameter, CultureInfo rpCulture) => rpValue.Equals(BooleanUtil.True) ? rpParameter : Binding.DoNothing;
+    }
+}
