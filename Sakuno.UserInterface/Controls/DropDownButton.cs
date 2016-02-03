1fcfa64207ae3f461ca8c8bf8b51e2d4538f846e
神樹桜乃 <kodama@sakuno.moe>
2016-02-03 21:12:48 +0800

    DropDownButton and SplitButton

---
+++ Sakuno.UserInterface/Controls/DropDownButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Sakuno.UserInterface.Controls
+{
+    public class DropDownButton : HeaderedContentControl
+    {
+        static DropDownButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DropDownButton), new FrameworkPropertyMetadata(typeof(DropDownButton)));
+        }
+    }
+}
---
+++ Sakuno.UserInterface/Controls/SplitButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Sakuno.UserInterface.Controls
+{
+    public class SplitButton : HeaderedContentControl
+    {
+        public static readonly DependencyProperty CommandProperty;
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty;
+        public object CommandParameter
+        {
+            get { return GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandTargetProperty;
+        public IInputElement CommandTarget
+        {
+            get { return (IInputElement)GetValue(CommandTargetProperty); }
+            set { SetValue(CommandTargetProperty, value); }
+        }
+
+        static SplitButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SplitButton), new FrameworkPropertyMetadata(typeof(SplitButton)));
+
+            CommandProperty = Button.CommandProperty.AddOwner(typeof(SplitButton));
+            CommandParameterProperty = Button.CommandParameterProperty.AddOwner(typeof(SplitButton));
+            CommandTargetProperty = Button.CommandTargetProperty.AddOwner(typeof(SplitButton));
+        }
+    }
+}
