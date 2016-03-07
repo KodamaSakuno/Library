2ec1c5e2ed8637a2e7ac0f195627e0777397da2c
神樹桜乃 <kodama@sakuno.moe>
2016-03-07 23:58:02 +0800

    Some extension methods of string

--- Sakuno.Base/StringExtensions.cs
+++ Sakuno.Base/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace Sakuno
@@ -12,6 +13,18 @@ namespace Sakuno
         public static string Join(this IEnumerable<string> rpStrings, string rpSeparator) => string.Join(rpSeparator, rpStrings);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool OICEquals(this string rpString, string rpValue) => rpString.Equals(rpValue, System.StringComparison.OrdinalIgnoreCase);
+        public static bool OICEquals(this string rpString, string rpValue) => rpString.Equals(rpValue, StringComparison.OrdinalIgnoreCase);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int OICIndexOf(this string rpString, string rpValue) => rpString.IndexOf(rpValue, StringComparison.OrdinalIgnoreCase);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int OICLastIndexOf(this string rpString, string rpValue) => rpString.LastIndexOf(rpValue, StringComparison.OrdinalIgnoreCase);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool OICContains(this string rpString, string rpValue) => rpString.OICIndexOf(rpValue) >= 0;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool OICStartsWith(this string rpString, string rpValue) => rpString.StartsWith(rpValue, StringComparison.OrdinalIgnoreCase);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool OICEndsWith(this string rpString, string rpValue) => rpString.EndsWith(rpValue, StringComparison.OrdinalIgnoreCase);
     }
 }
