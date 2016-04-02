5520ac0cc4d61f4d5fe892dc03da719598e78fcd
神樹桜乃 <kodama@sakuno.moe>
2016-04-02 09:23:15 +0800

    Add generic version of weak reference for Fx4

---
+++ Sakuno.Base/WeakReference`T.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Sakuno
+{
+    public class WeakReference<T> : WeakReference where T : class
+    {
+        public WeakReference(object rpTarget) : base(rpTarget) { }
+        public WeakReference(object rpTarget, bool rpTrackResurrection) : base(rpTarget, rpTrackResurrection) { }
+        protected WeakReference(SerializationInfo rpInfo, StreamingContext rpContext) : base(rpInfo, rpContext) { }
+
+        public bool TryGetTarget(out T ropTarget)
+        {
+            ropTarget = Target as T;
+            return ropTarget != null;
+        }
+        public void SetTarget(T rpTarget) => Target = rpTarget;
+    }
+}
