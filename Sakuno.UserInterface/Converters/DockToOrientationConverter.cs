61ec90f5ef8acefedd67a7bc4e87d6acbf65c5c2
神樹桜乃 <kodama@sakuno.moe>
2016-04-10 08:45:17 +0800

    A dock to orientation converter

---
+++ Sakuno.UserInterface/Converters/DockToOrientationConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Sakuno.UserInterface.Converters
+{
+    [ValueConversion(typeof(Dock), typeof(Orientation))]
+    public class DockToOrientationConverter : IValueConverter
+    {
+        public object Convert(object rpValue, Type rpTargetType, object rpParameter, CultureInfo rpCulture)
+        {
+            var rDock = (Dock)rpValue;
+            return rDock == Dock.Left || rDock == Dock.Right ? Orientation.Horizontal : Orientation.Vertical;
+        }
+
+        public object ConvertBack(object rpValue, Type rpTargetType, object rpParameter, CultureInfo rpCulture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
