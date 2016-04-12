c434579dca16b2e76082dd5076d00312d996b812
神樹桜乃 <kodama@sakuno.moe>
2016-04-12 20:07:14 +0800

    Add PlaySound()

--- Sakuno.SystemInterop/NativeEnums.cs
+++ Sakuno.SystemInterop/NativeEnums.cs
@@ -132,5 +132,29 @@ namespace Sakuno.SystemInterop
             FLASHW_TIMER = 4,
             FLASHW_TIMERNOFG = 12,
         }
+
+        [Flags]
+        public enum SND
+        {
+            SND_SYNC,
+            SND_ASYNC = 0x0001,
+            SND_NODEFAULT = 0x0002,
+            SND_MEMORY = 0x0004,
+            SND_LOOP = 0x0008,
+            SND_NOSTOP = 0x0010,
+
+            SND_NOWAIT = 0x00002000,
+            SND_ALIAS = 0x00010000,
+            SND_ALIAS_ID = 0x00110000,
+            SND_FILENAME = 0x00020000,
+            SND_RESOURCE = 0x00040004,
+
+            SND_PURGE = 0x0040,
+            SND_APPLICATION = 0x0080,
+
+            SND_SENTRY = 0X00080000,
+            SND_RING = 0X00100000,
+            SND_SYSTEM = 0X00200000,
+        }
     }
 }
