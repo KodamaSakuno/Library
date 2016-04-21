bb5ceebe13c98b5f8a08c7872d7cf8fc6164f704
神樹桜乃 <kodama@sakuno.moe>
2016-04-21 10:01:20 +0800

    Add AreEqualConverter

---
+++ Sakuno.Base/Converters/AreEqualConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Sakuno.Converters
+{
+    public class AreEqualConverter : IMultiValueConverter
+    {
+        public object Convert(object[] rpValues, Type rpTargetType, object rpParameter, CultureInfo rpCulture)
+        {
+            if (rpValues == null || rpValues.Length < 2)
+                throw new ArgumentException(nameof(rpValues));
+
+            return Equals(rpValues[0], rpValues[1]);
+        }
+
+        public object[] ConvertBack(object rpValue, Type[] rpTargetType, object rpParameter, CultureInfo rpCulture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
