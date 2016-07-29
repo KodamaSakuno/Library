e2c2cdf71fb95d2cd9ce8b07a0e906bdc1e155fc
神樹桜乃 <kodama@sakuno.moe>
2016-07-29 11:27:13 +0800

    Simple BBCodeBlock

---
+++ Sakuno.UserInterface/Controls/BBCodeBlock.cs
@@ -0,0 +1,47 @@
+﻿using Sakuno.Collections;
+using Sakuno.UserInterface.Internal;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Markup;
+
+namespace Sakuno.UserInterface.Controls
+{
+    [ContentProperty("BBCode")]
+    public class BBCodeBlock : TextBlock
+    {
+        public static readonly DependencyProperty BBCodeProperty = DependencyProperty.Register(nameof(BBCode), typeof(string), typeof(BBCodeBlock),
+            new PropertyMetadata((s, e) => ((BBCodeBlock)s).Update(true)));
+        public string BBCode
+        {
+            get { return (string)GetValue(BBCodeProperty); }
+            set { SetValue(BBCodeProperty, value); }
+        }
+
+        internal static IDictionary<string, Func<Inline, Inline>> CustomTags { get; } = new HybridDictionary<string, Func<Inline, Inline>>(StringComparer.OrdinalIgnoreCase);
+
+        bool r_IsUpdating;
+
+        public BBCodeBlock()
+        {
+            AddHandler(LoadedEvent, new RoutedEventHandler((s, e) => Update(false)));
+        }
+        void Update(bool rpForceUpdate)
+        {
+            if (rpForceUpdate)
+                r_IsUpdating = true;
+
+            if (!IsLoaded || !r_IsUpdating)
+                return;
+
+            Inlines.Clear();
+            Inlines.Add(BBCodeParsers.Parse(BBCode));
+
+            r_IsUpdating = false;
+        }
+
+        public static void AddCustomTag(string rpTag, Func<Inline, Inline> rpInlineConstructor) => CustomTags.Add(rpTag, rpInlineConstructor);
+    }
+}
---
+++ Sakuno.UserInterface/Internal/BBCodeParser.cs
@@ -0,0 +1,68 @@
+﻿using Sakuno.Parsers;
+using Sakuno.UserInterface.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Documents;
+
+namespace Sakuno.UserInterface.Internal
+{
+    using Parser = Parser<IInput<char>, Inline>;
+
+    static class BBCodeParsers
+    {
+        static Parser SimpleText =>
+            from rText in StringParsers.AnyChar.Until(StringParsers.Char('[')).AsString()
+            select new Run(rText);
+
+        static Parser<IInput<char>, string> OpeningTag =>
+            from _ in StringParsers.Char('[')
+            from rName in StringParsers.Letter.AtLeastOnce().AsString()
+            from __ in StringParsers.Char(']')
+            select rName;
+        static Parser Tag =>
+            from rOpeningTag in OpeningTag
+            from rSubBBCode in BBCode
+            from _ in StringParsers.Char('[')
+            from __ in StringParsers.Char('/')
+            from rClosingTag in StringParsers.String(rOpeningTag)
+            from ___ in StringParsers.Char(']')
+            select GetInline(rOpeningTag, rSubBBCode);
+
+        static Parser BBCode => Tag.LeftIfPartiallyParsed(SimpleText).Many().Select(GetSpan);
+
+        static Inline GetInline(string rpOpeningTag, Inline rpContent)
+        {
+            if (rpOpeningTag.OICEquals("b"))
+                return new Bold(rpContent);
+            else if (rpOpeningTag.OICEquals("i"))
+                return new Italic(rpContent);
+            else if (rpOpeningTag.OICEquals("u"))
+                return new Underline(rpContent);
+
+            Func<Inline, Inline> rConsturctor;
+            if (BBCodeBlock.CustomTags.TryGetValue(rpOpeningTag, out rConsturctor))
+                return rConsturctor(rpContent) ?? rpContent;
+
+            return rpContent;
+        }
+        static Inline GetSpan(IEnumerable<Inline> rpInlines)
+        {
+            var rSpan = new Span();
+
+            foreach (var rInline in rpInlines)
+                rSpan.Inlines.Add(rInline);
+
+            return rSpan;
+        }
+
+        public static Inline Parse(string rpBBCode)
+        {
+            var rResult = BBCode(new StringInput(rpBBCode));
+            if (!rResult.Success)
+                return new Run(rpBBCode);
+
+            return rResult.Value;
+        }
+    }
+}
