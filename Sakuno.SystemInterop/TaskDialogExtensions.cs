e5f1d7c7ca08c55f072f49fb080bc20b61672c4e
神樹桜乃 <kodama@sakuno.moe>
2016-08-03 13:21:16 +0800

    Little extension method

---
+++ Sakuno.SystemInterop/TaskDialogExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Sakuno.SystemInterop
+{
+    public static class TaskDialogExtensions
+    {
+        public static TaskDialogResult ShowAndDispose(this TaskDialog rpTaskDialog)
+        {
+            using (rpTaskDialog)
+                return rpTaskDialog.Show();
+        }
+    }
+}
