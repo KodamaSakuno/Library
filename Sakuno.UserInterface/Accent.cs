49d121b4abaab1cf22dab7d9893e2ca3aa4ff1a9
神樹桜乃 <kodama@sakuno.moe>
2016-09-19 17:05:19 +0800

    Little modification

--- Sakuno.UserInterface/Accent.cs
+++ Sakuno.UserInterface/Accent.cs
@@ -6,13 +6,13 @@ namespace Sakuno.UserInterface
     public class Accent
     {
         public static Accent Blue { get; } = new Accent("Blue", new Uri("pack://application:,,,/Sakuno.UserInterface;component/Themes/Accents/Blue.xaml"));
-        public static Accent Brown { get; } = new Accent("Blue", new Uri("pack://application:,,,/Sakuno.UserInterface;component/Themes/Accents/Brown.xaml"));
+        public static Accent Brown { get; } = new Accent("Brown", new Uri("pack://application:,,,/Sakuno.UserInterface;component/Themes/Accents/Brown.xaml"));
 
         public string Name { get; }
 
         public ResourceDictionary ResourceDictionary { get; }
 
-        Accent(string rpName, Uri rpUri)
+        public Accent(string rpName, Uri rpUri)
         {
             Name = rpName;
 
