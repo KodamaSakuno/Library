5a09f95623ee924540b8f0261019a8363928dd9c
神樹桜乃 <kodama@sakuno.moe>
2016-10-03 21:32:14 +0800

    Generic data template selector

---
+++ Sakuno.UserInterface/DataTemplateSelection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace Sakuno.UserInterface
+{
+    [ContentProperty("DataTemplate")]
+    public class DataTemplateSelection
+    {
+        public object EqualsTo { get; set; }
+
+        public Type DataType { get; set; }
+
+        public DataTemplate DataTemplate { get; set; }
+    }
+}
---
+++ Sakuno.UserInterface/GenericDataTemplateSelector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace Sakuno.UserInterface
+{
+    [ContentProperty("Selection")]
+    public class GenericDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate Default { get; set; }
+
+        Collection<DataTemplateSelection> r_Selection;
+        public Collection<DataTemplateSelection> Selection
+        {
+            get
+            {
+                if (r_Selection == null)
+                    r_Selection = new Collection<DataTemplateSelection>();
+                return r_Selection;
+            }
+        }
+
+        public override DataTemplate SelectTemplate(object rpItem, DependencyObject rpContainer)
+        {
+            if (r_Selection != null && rpItem != null)
+            {
+                var rType = rpItem.GetType();
+
+                foreach (var rSelection in r_Selection)
+                    if (rpItem == rSelection.EqualsTo || rType == rSelection.DataType)
+                        return rSelection.DataTemplate;
+            }
+
+            return Default ?? base.SelectTemplate(rpItem, rpContainer);
+        }
+    }
+}
