cba9b81f7f425d83917fc97928c04eda396a0153
神樹桜乃 <kodama@sakuno.moe>
2016-12-10 20:07:01 +0800

    Two literal extension

---
+++ Sakuno.UserInterface/DoubleExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace Sakuno.UserInterface
+{
+    public sealed class DoubleExtension : MarkupExtension
+    {
+        public double Value { get; set; }
+
+        public DoubleExtension(double rpValue)
+        {
+            Value = rpValue;
+        }
+
+        public override object ProvideValue(IServiceProvider rpServiceProvider)
+        {
+            if (DoubleUtil.IsZero(Value))
+                return DoubleUtil.Zero;
+            else if (DoubleUtil.IsOne(Value))
+                return DoubleUtil.One;
+            else if (Value.IsNaN())
+                return DoubleUtil.NaN;
+
+            return Value;
+        }
+    }
+}
---
+++ Sakuno.UserInterface/IntegerExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace Sakuno.UserInterface
+{
+    public sealed class IntegerExtension : MarkupExtension
+    {
+        public int Value { get; set; }
+
+        public IntegerExtension(int rpValue)
+        {
+            Value = rpValue;
+        }
+
+        public override object ProvideValue(IServiceProvider rpServiceProvider)
+        {
+            switch (Value)
+            {
+                case 0: return Int32Util.Zero;
+
+                default: return Value;
+            }
+        }
+    }
+}
