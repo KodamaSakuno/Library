a39eb19e0c91a020da8713a2c4fb9e2d707ccba6
神樹桜乃 <kodama@sakuno.moe>
2016-12-14 20:56:12 +0800

    An extension method

---
+++ Sakuno.Base/Collections/DictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Sakuno.Collections
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> rpDictionary, TKey rpKey) where TValue : class
+        {
+            TValue rResult;
+            rpDictionary.TryGetValue(rpKey, out rResult);
+            return rResult;
+        }
+    }
+}
