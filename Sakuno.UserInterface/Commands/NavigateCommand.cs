a301d657169a1f08d0ff85623ad2bc6264aecbb2
神樹桜乃 <kodama@sakuno.moe>
2016-12-14 18:54:27 +0800

    A command to navigate to another page

---
+++ Sakuno.UserInterface/Commands/NavigateCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Sakuno.UserInterface.Commands
+{
+    public class NavigateCommand : Freezable, ICommand
+    {
+        public static readonly DependencyProperty TargetPageProperty = DependencyProperty.Register(nameof(TargetPage), typeof(Page), typeof(NavigateCommand),
+            new PropertyMetadata(null));
+        public Page TargetPage
+        {
+            get { return (Page)GetValue(TargetPageProperty); }
+            set { SetValue(TargetPageProperty, value); }
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public NavigateCommand()
+        {
+            BindingOperations.SetBinding(this, TargetPageProperty, new Binding() { RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(Page), 1) });
+        }
+
+        public void Execute(object rpParameter)
+        {
+            var rPage = TargetPage;
+            if (rPage == null)
+                return;
+
+            var rUri = rpParameter as Uri;
+            if (rUri == null)
+            {
+                var rUriString = rpParameter as string;
+                if (rUriString != null)
+                    rUri = new Uri(rUriString, UriKind.RelativeOrAbsolute);
+            }
+
+            if (rUri != null)
+            {
+                rPage.NavigationService.Navigate(rUri);
+                return;
+            }
+
+            rPage.NavigationService.Navigate(rpParameter);
+        }
+
+        public bool CanExecute(object rpParameter) => true;
+
+        protected override Freezable CreateInstanceCore() => new NavigateCommand();
+    }
+}
---
+++ Sakuno.UserInterface/Commands/NavigateCommandExtension.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace Sakuno.UserInterface.Commands
+{
+    public class NavigateCommandExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider rpServiceProvider) => new NavigateCommand();
+    }
+}
