33d666f7bdcd0dc9110d6ba4307fcf62d203afbb
神樹桜乃 <kodama@sakuno.moe>
2016-12-30 13:43:01 +0800

    Property changed trigger

---
+++ Sakuno.UserInterface/Interactivity/PropertyChangedTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace Sakuno.UserInterface.Interactivity
+{
+    public class PropertyChangedTrigger : TriggerBase<DependencyObject>
+    {
+        public static readonly DependencyProperty BindingProperty = DependencyProperty.Register(nameof(Binding), typeof(object), typeof(PropertyChangedTrigger),
+            new PropertyMetadata((s, e) => ((PropertyChangedTrigger)s).OnBindingChanged(e)));
+        public object Binding
+        {
+            get { return GetValue(BindingProperty); }
+            set { SetValue(BindingProperty, value); }
+        }
+
+        public static readonly DependencyProperty ThrottleDueTimeProperty = DependencyProperty.Register(nameof(ThrottleDueTime), typeof(TimeSpan), typeof(PropertyChangedTrigger),
+            new PropertyMetadata(TimeSpan.Zero));
+        public TimeSpan ThrottleDueTime
+        {
+            get { return (TimeSpan)GetValue(ThrottleDueTimeProperty); }
+            set { SetValue(ThrottleDueTimeProperty, value); }
+        }
+
+        int r_Count;
+
+        void OnBindingChanged(DependencyPropertyChangedEventArgs e)
+        {
+            var rThrottleDueTime = ThrottleDueTime;
+            if (rThrottleDueTime == TimeSpan.Zero)
+                InvokeActions(e);
+            else
+                ThrottleCore(rThrottleDueTime, e);
+        }
+        async void ThrottleCore(TimeSpan rpDueTime, DependencyPropertyChangedEventArgs e)
+        {
+            Interlocked.Increment(ref r_Count);
+
+            await Task.Delay(rpDueTime);
+
+            if (Interlocked.Decrement(ref r_Count) == 0)
+                InvokeActions(e);
+        }
+    }
+}
