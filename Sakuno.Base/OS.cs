e36535f6cea2609d5246f4352043399310e5171c
神樹桜乃 <kodama@sakuno.moe>
2017-01-22 22:43:22 +0800

    Property to query .Net Framework release number

--- Sakuno.Base/OS.cs
+++ Sakuno.Base/OS.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 
 namespace Sakuno
 {
@@ -14,5 +15,27 @@ namespace Sakuno
         public static bool IsWin8OrLater => r_OS.Version.Major >= 7 || (r_OS.Version.Major == 6 && r_OS.Version.Minor >= 2);
         public static bool IsWin81OrLater => r_OS.Version.Major >= 7 || (r_OS.Version.Major == 6 && r_OS.Version.Minor >= 3);
         public static bool IsWin10OrLater => r_OS.Version.Major >= 10;
+
+        public static int? DotNetFrameworkReleaseNumber
+        {
+            get
+            {
+                const string rSubKey = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\";
+
+                try
+                {
+                    using (var rRegisterKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(rSubKey))
+                    {
+                        var rValue = rRegisterKey?.GetValue("Release");
+
+                        return rValue != null ? (int?)rValue : null;
+                    }
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
     }
 }
