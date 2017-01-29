e04fa6625d82579f96f6af4d81743d88d110ee1e
神樹桜乃 <kodama@sakuno.moe>
2017-01-29 19:19:50 +0800

    Fast DateTime operations

---
+++ Sakuno.Base/DateTimeExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Sakuno
+{
+    public static class DateTimeExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static DateTimeOffset AsOffset(this DateTime rpDateTime) => new DateTimeOffset(rpDateTime);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static DateTimeOffset DateAsOffset(this DateTimeOffset rpDateTime) => new DateTimeOffset(rpDateTime.Date, rpDateTime.Offset);
+
+        public static DateTime Tomorrow(this DateTime rpDateTime) =>
+            new DateTime(TomorrowTicks(rpDateTime.Ticks), rpDateTime.Kind);
+        public static DateTimeOffset Tomorrow(this DateTimeOffset rpDateTime) =>
+            new DateTimeOffset(TomorrowTicks(rpDateTime.Ticks), rpDateTime.Offset);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static long TomorrowTicks(long rpTicks) => rpTicks - rpTicks % DateTimeUtil.TicksPerDay + DateTimeUtil.TicksPerDay;
+
+        public static DateTime LastMonday(this DateTime rpDateTime) =>
+            new DateTime(LastMondayTicks(rpDateTime.Ticks), rpDateTime.Kind);
+        public static DateTimeOffset LastMonday(this DateTimeOffset rpDateTime) =>
+            new DateTimeOffset(LastMondayTicks(rpDateTime.Ticks), rpDateTime.Offset);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static long LastMondayTicks(long rpTicks)
+        {
+            var rDays = (int)(rpTicks / DateTimeUtil.TicksPerDay);
+
+            return (rDays - rDays % 7) * DateTimeUtil.TicksPerDay;
+        }
+
+        public static DateTime NextMonday(this DateTime rpDateTime) =>
+            new DateTime(NextMondayTicks(rpDateTime.Ticks), rpDateTime.Kind);
+        public static DateTimeOffset NextMonday(this DateTimeOffset rpDateTime) =>
+            new DateTimeOffset(NextMondayTicks(rpDateTime.Ticks), rpDateTime.Offset);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static long NextMondayTicks(long rpTicks)
+        {
+            var rDays = (int)(rpTicks / DateTimeUtil.TicksPerDay);
+
+            return (rDays - rDays % 7 + 7) * DateTimeUtil.TicksPerDay;
+        }
+
+        public static DateTime StartOfLastMonth(this DateTime rpDateTime) =>
+            new DateTime(StartOfLastMonthTicks(rpDateTime.Ticks), rpDateTime.Kind);
+        public static DateTimeOffset StartOfLastMonth(this DateTimeOffset rpDateTime) =>
+            new DateTimeOffset(StartOfLastMonthTicks(rpDateTime.Ticks), rpDateTime.Offset);
+        static long StartOfLastMonthTicks(long rpTicks)
+        {
+            int rYear, rMonth, rDay;
+            bool rIsLeapYear;
+            DateTimeUtil.ExtractDate(rpTicks, out rYear, out rMonth, out rDay, out rIsLeapYear);
+
+            if (rDay > 1)
+                rpTicks -= (rDay - 1) * DateTimeUtil.TicksPerDay;
+
+            return rpTicks - rpTicks % DateTimeUtil.TicksPerDay;
+        }
+
+        public static DateTime StartOfNextMonth(this DateTime rpDateTime) =>
+            new DateTime(StartOfNextMonthTicks(rpDateTime.Ticks), rpDateTime.Kind);
+        public static DateTimeOffset StartOfNextMonth(this DateTimeOffset rpDateTime) =>
+            new DateTimeOffset(StartOfNextMonthTicks(rpDateTime.Ticks), rpDateTime.Offset);
+        static long StartOfNextMonthTicks(long rpTicks)
+        {
+            int rYear, rMonth, rDay;
+            bool rIsLeapYear;
+            DateTimeUtil.ExtractDate(rpTicks, out rYear, out rMonth, out rDay, out rIsLeapYear);
+
+            var rDays = rIsLeapYear ? DateTimeUtil.DaysToMonth366 : DateTimeUtil.DaysToMonth365;
+
+            rDay = rDays[rMonth] - rDays[rMonth - 1] - rDay + 1;
+            rpTicks += rDay * DateTimeUtil.TicksPerDay;
+
+            return rpTicks - rpTicks % DateTimeUtil.TicksPerDay;
+        }
+    }
+}
--- Sakuno.Base/DateTimeUtil.cs
+++ Sakuno.Base/DateTimeUtil.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Sakuno
 {
@@ -7,9 +8,61 @@ namespace Sakuno
         public const long UnixEpochSeconds = 62135596800L;
         public const long UnixEpochTicks = 621355968000000000L;
 
+        public const long TicksPerDay = 864000000000L;
+
+        public const int DaysPerYear = 365;
+        public const int DaysPer4Years = DaysPerYear * 4 + 1;
+        public const int DaysPer100Years = DaysPer4Years * 25 - 1;
+        public const int DaysPer400Years = DaysPer100Years * 4 + 1;
+
+        internal static readonly int[] DaysToMonth365, DaysToMonth366;
+
         public static DateTimeOffset UnixEpoch { get; } = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+        static DateTimeUtil()
+        {
+            var rType = typeof(DateTime);
+            var rFlags = BindingFlags.NonPublic | BindingFlags.Static;
+
+            DaysToMonth365 = (int[])rType.GetField(nameof(DaysToMonth365), rFlags).GetValue(null);
+            DaysToMonth366 = (int[])rType.GetField(nameof(DaysToMonth366), rFlags).GetValue(null);
+        }
 
         public static DateTimeOffset FromUnixTime(long rpValue) => new DateTimeOffset(rpValue * 10000000 + UnixEpochTicks, TimeSpan.Zero);
         public static long ToUnixTime(this DateTimeOffset rpDateTime) => rpDateTime.UtcDateTime.Ticks / 10000000 - UnixEpochSeconds;
+
+        internal static void ExtractDate(long rpTicks, out int ropYear, out int ropMonth, out int ropDay, out bool ropIsLeapYear)
+        {
+            var rpTotalDays = (int)(rpTicks / TicksPerDay);
+
+            var rPeriod400Years = rpTotalDays / DaysPer400Years;
+            rpTotalDays -= rPeriod400Years * DaysPer400Years;
+
+            var rPeriod100Years = rpTotalDays / DaysPer100Years;
+            if (rPeriod100Years == 4)
+                rPeriod100Years = 3;
+            rpTotalDays -= rPeriod100Years * DaysPer100Years;
+
+            var rPeriod4Years = rpTotalDays / DaysPer4Years;
+            rpTotalDays -= rPeriod4Years * DaysPer4Years;
+
+            var rPeriod1Year = rpTotalDays / DaysPerYear;
+            if (rPeriod1Year == 4)
+                rPeriod1Year = 3;
+
+            ropYear = rPeriod400Years * 400 + rPeriod100Years * 100 + rPeriod4Years * 4 + rPeriod1Year + 1;
+
+            rpTotalDays -= rPeriod1Year * DaysPerYear;
+
+            ropIsLeapYear = rPeriod1Year == 3 && (rPeriod4Years != 24 || rPeriod100Years == 3);
+
+            var rDays = ropIsLeapYear ? DaysToMonth366 : DaysToMonth365;
+
+            ropMonth = rpTotalDays >> 5 + 1;
+            while (rpTotalDays >= rDays[ropMonth])
+                ropMonth++;
+
+            ropDay = rpTotalDays - rDays[ropMonth - 1] + 1;
+        }
     }
 }
