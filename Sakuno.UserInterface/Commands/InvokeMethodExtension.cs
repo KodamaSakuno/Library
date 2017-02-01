c6fcda39122456f15c595c51245636c8da45bcf7
神樹桜乃 <kodama@sakuno.moe>
2017-02-01 23:22:30 +0800

    Invoke method extension

---
+++ Sakuno.UserInterface/Commands/InvokeMethodExtension.cs
@@ -0,0 +1,30 @@
+﻿using Sakuno.UserInterface.ObjectOperations;
+using System;
+using System.Windows.Markup;
+
+namespace Sakuno.UserInterface.Commands
+{
+    public class InvokeMethodExtension : MarkupExtension
+    {
+        string r_Method;
+
+        object r_Parameter;
+
+        public InvokeMethodExtension(string rpMethod) : this(rpMethod, null) { }
+        public InvokeMethodExtension(string rpMethod, object rpParameter)
+        {
+            r_Method = rpMethod;
+            r_Parameter = rpParameter;
+        }
+
+        public override object ProvideValue(IServiceProvider rpServiceProvider)
+        {
+            var rInvokeMethod = new InvokeMethod() { Method = r_Method };
+
+            if (r_Parameter != null)
+                rInvokeMethod.Parameters.Add(new MethodParameter() { Value = r_Parameter });
+
+            return new ObjectOperationCommand() { Operations = { rInvokeMethod } };
+        }
+    }
+}
