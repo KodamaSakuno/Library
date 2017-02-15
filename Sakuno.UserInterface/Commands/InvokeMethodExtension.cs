e196df6d5b9972435af8ec9f24655164a3ece9cc
神樹桜乃 <kodama@sakuno.moe>
2017-02-15 20:43:02 +0800

    Add target property for InvokeMethod markup extension

--- Sakuno.UserInterface/Commands/InvokeMethodExtension.cs
+++ Sakuno.UserInterface/Commands/InvokeMethodExtension.cs
@@ -11,6 +11,8 @@ namespace Sakuno.UserInterface.Commands
 
         object r_Parameter;
 
+        public object Target { get; set; }
+
         public InvokeMethodExtension(string rpMethod) : this(rpMethod, null) { }
         public InvokeMethodExtension(string rpMethod, object rpParameter)
         {
@@ -33,6 +35,15 @@ namespace Sakuno.UserInterface.Commands
                     BindingOperations.SetBinding(rParameter, MethodParameter.ValueProperty, rBinding);
 
                 rInvokeMethod.Parameters.Add(rParameter);
+            }
+
+            if (Target != null)
+            {
+                var rBinding = Target as Binding;
+                if (rBinding != null)
+                    BindingOperations.SetBinding(rInvokeMethod, InvokeMethod.TargetProperty, rBinding);
+                else
+                    rInvokeMethod.Target = Target;
             }
 
             return new ObjectOperationCommand() { Operations = { rInvokeMethod } };
