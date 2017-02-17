6f1c10c6fed6568022dee6aa5ac2f8800ef23af5
神樹桜乃 <kodama@sakuno.moe>
2017-02-17 11:32:34 +0800

    More memory operations

--- Sakuno.Base/Memory.cs
+++ Sakuno.Base/Memory.cs
@@ -2,7 +2,6 @@
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace Sakuno
 {
@@ -44,39 +43,143 @@ namespace Sakuno
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Clear(void* rpBuffer, int rpCount) => r_MemorySet(rpBuffer, 0, rpCount);
+        public static void Clear(void* rpPointer, int rpCount) => r_MemorySet(rpPointer, 0, rpCount);
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Clear(IntPtr rpBuffer, int rpCount) => r_MemorySet((void*)rpBuffer, 0, rpCount);
-        public static void Clear(byte[] rpBuffer, int rpCount)
+        public static void Clear(IntPtr rpPointer, int rpCount) => r_MemorySet((void*)rpPointer, 0, rpCount);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Clear(byte[] rpArray, int rpCount)
         {
-            var rHandle = GCHandle.Alloc(rpBuffer, GCHandleType.Pinned);
-            try
-            {
-                Clear(rHandle.AddrOfPinnedObject(), rpCount);
-            }
-            finally
-            {
-                rHandle.Free();
-            }
+            fixed (void* rPointer = rpArray)
+                r_MemorySet(rPointer, 0, rpCount);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Clear(bool[] rpArray, int rpCount)
+        {
+            fixed (void* rPointer = rpArray)
+                r_MemorySet(rPointer, 0, rpCount * sizeof(bool));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Clear(char[] rpArray, int rpCount)
+        {
+            fixed (void* rPointer = rpArray)
+                r_MemorySet(rPointer, 0, rpCount * sizeof(char));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Clear(short[] rpArray, int rpCount)
+        {
+            fixed (void* rPointer = rpArray)
+                r_MemorySet(rPointer, 0, rpCount * sizeof(short));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Clear(int[] rpArray, int rpCount)
+        {
+            fixed (void* rPointer = rpArray)
+                r_MemorySet(rPointer, 0, rpCount * sizeof(int));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Clear(uint[] rpArray, int rpCount)
+        {
+            fixed (void* rPointer = rpArray)
+                r_MemorySet(rPointer, 0, rpCount * sizeof(uint));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Clear(long[] rpArray, int rpCount)
+        {
+            fixed (void* rPointer = rpArray)
+                r_MemorySet(rPointer, 0, rpCount * sizeof(long));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Clear(ulong[] rpArray, int rpCount)
+        {
+            fixed (void* rPointer = rpArray)
+                r_MemorySet(rPointer, 0, rpCount * sizeof(ulong));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Clear(float[] rpArray, int rpCount)
+        {
+            fixed (void* rPointer = rpArray)
+                r_MemorySet(rPointer, 0, rpCount * sizeof(float));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Clear(double[] rpArray, int rpCount)
+        {
+            fixed (void* rPointer = rpArray)
+                r_MemorySet(rPointer, 0, rpCount * sizeof(double));
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Copy(void* rpSource, void* rpDestination, int rpCount) => r_MemoryCopy(rpDestination, rpSource, rpCount);
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Copy(IntPtr rpSource, IntPtr rpDestination, int rpCount) => r_MemoryCopy((void*)rpDestination, (void*)rpSource, rpCount);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Copy(byte[] rpSource, byte[] rpDestination, int rpCount)
         {
-            var rSourceHandle = GCHandle.Alloc(rpSource, GCHandleType.Pinned);
-            var rDestinationHandle = GCHandle.Alloc(rpDestination, GCHandleType.Pinned);
-            try
-            {
-                Copy(rSourceHandle.AddrOfPinnedObject(), rDestinationHandle.AddrOfPinnedObject(), rpCount);
-            }
-            finally
-            {
-                rSourceHandle.Free();
-                rDestinationHandle.Free();
-            }
+            fixed (void* rSource = rpSource)
+            fixed (void* rDestination = rpDestination)
+                r_MemoryCopy(rDestination, rSource, rpCount);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Copy(bool[] rpSource, bool[] rpDestination, int rpCount)
+        {
+            fixed (void* rSource = rpSource)
+            fixed (void* rDestination = rpDestination)
+                r_MemoryCopy(rDestination, rSource, rpCount * sizeof(bool));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Copy(char[] rpSource, char[] rpDestination, int rpCount)
+        {
+            fixed (void* rSource = rpSource)
+            fixed (void* rDestination = rpDestination)
+                r_MemoryCopy(rDestination, rSource, rpCount * sizeof(char));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Copy(short[] rpSource, short[] rpDestination, int rpCount)
+        {
+            fixed (void* rSource = rpSource)
+            fixed (void* rDestination = rpDestination)
+                r_MemoryCopy(rDestination, rSource, rpCount * sizeof(short));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Copy(int[] rpSource, int[] rpDestination, int rpCount)
+        {
+            fixed (void* rSource = rpSource)
+            fixed (void* rDestination = rpDestination)
+                r_MemoryCopy(rDestination, rSource, rpCount * sizeof(int));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Copy(uint[] rpSource, uint[] rpDestination, int rpCount)
+        {
+            fixed (void* rSource = rpSource)
+            fixed (void* rDestination = rpDestination)
+                r_MemoryCopy(rDestination, rSource, rpCount * sizeof(uint));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Copy(long[] rpSource, long[] rpDestination, int rpCount)
+        {
+            fixed (void* rSource = rpSource)
+            fixed (void* rDestination = rpDestination)
+                r_MemoryCopy(rDestination, rSource, rpCount * sizeof(long));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Copy(ulong[] rpSource, ulong[] rpDestination, int rpCount)
+        {
+            fixed (void* rSource = rpSource)
+            fixed (void* rDestination = rpDestination)
+                r_MemoryCopy(rDestination, rSource, rpCount * sizeof(ulong));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Copy(float[] rpSource, float[] rpDestination, int rpCount)
+        {
+            fixed (void* rSource = rpSource)
+            fixed (void* rDestination = rpDestination)
+                r_MemoryCopy(rDestination, rSource, rpCount * sizeof(float));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Copy(double[] rpSource, double[] rpDestination, int rpCount)
+        {
+            fixed (void* rSource = rpSource)
+            fixed (void* rDestination = rpDestination)
+                r_MemoryCopy(rDestination, rSource, rpCount * sizeof(double));
         }
 
         delegate void MemorySet(void* rpAddress, byte rpValue, int rpCount);
