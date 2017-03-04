a0e78bb893df0325fe7226736c61b11e1705e619
神樹桜乃 <kodama@sakuno.moe>
2017-03-04 20:47:09 +0800

    Add ToSortedList() extension methods

--- Sakuno.Base/EnumerableExtensions.cs
+++ Sakuno.Base/EnumerableExtensions.cs
@@ -23,9 +23,9 @@ namespace Sakuno
             }
         }
 
-        public static HybridDictionary<TKey, TSource> ToHybridDictionary<TSource, TKey>(this IEnumerable<TSource> rpSource, Func<TSource, TKey> rpKeySelector) => rpSource.ToHybridDictionary(rpKeySelector, IdentityFunction<TSource>.Instance, null);
+        public static HybridDictionary<TKey, TSource> ToHybridDictionary<TSource, TKey>(this IEnumerable<TSource> rpSource, Func<TSource, TKey> rpKeySelector) => rpSource.ToHybridDictionary(rpKeySelector, IdentityFunction<TSource>.Instance, EqualityComparer<TKey>.Default);
         public static HybridDictionary<TKey, TSource> ToHybridDictionary<TSource, TKey>(this IEnumerable<TSource> rpSource, Func<TSource, TKey> rpKeySelector, IEqualityComparer<TKey> rpComparer) => rpSource.ToHybridDictionary(rpKeySelector, IdentityFunction<TSource>.Instance, rpComparer);
-        public static HybridDictionary<TKey, TElement> ToHybridDictionary<TSource, TKey, TElement>(this IEnumerable<TSource> rpSource, Func<TSource, TKey> rpKeySelector, Func<TSource, TElement> rpElementSelector) => rpSource.ToHybridDictionary(rpKeySelector, rpElementSelector, null);
+        public static HybridDictionary<TKey, TElement> ToHybridDictionary<TSource, TKey, TElement>(this IEnumerable<TSource> rpSource, Func<TSource, TKey> rpKeySelector, Func<TSource, TElement> rpElementSelector) => rpSource.ToHybridDictionary(rpKeySelector, rpElementSelector, EqualityComparer<TKey>.Default);
         public static HybridDictionary<TKey, TElement> ToHybridDictionary<TSource, TKey, TElement>(this IEnumerable<TSource> rpSource, Func<TSource, TKey> rpKeySelector, Func<TSource, TElement> rpElementSelector, IEqualityComparer<TKey> rpComparer)
         {
             if (rpSource == null)
@@ -36,6 +36,25 @@ namespace Sakuno
                 throw new ArgumentNullException(nameof(rpElementSelector));
 
             var rResult = new HybridDictionary<TKey, TElement>(rpComparer);
+            foreach (var rItem in rpSource)
+                rResult.Add(rpKeySelector(rItem), rpElementSelector(rItem));
+
+            return rResult;
+        }
+
+        public static SortedList<TKey, TSource> ToSortedList<TSource, TKey>(this IEnumerable<TSource> rpSource, Func<TSource, TKey> rpKeySelector) => rpSource.ToSortedList(rpKeySelector, IdentityFunction<TSource>.Instance, Comparer<TKey>.Default);
+        public static SortedList<TKey, TSource> ToSortedList<TSource, TKey>(this IEnumerable<TSource> rpSource, Func<TSource, TKey> rpKeySelector, IComparer<TKey> rpComparer) => rpSource.ToSortedList(rpKeySelector, IdentityFunction<TSource>.Instance, rpComparer);
+        public static SortedList<TKey, TElement> ToSortedList<TSource, TKey, TElement>(this IEnumerable<TSource> rpSource, Func<TSource, TKey> rpKeySelector, Func<TSource, TElement> rpElementSelector) => rpSource.ToSortedList(rpKeySelector, rpElementSelector, Comparer<TKey>.Default);
+        public static SortedList<TKey, TElement> ToSortedList<TSource, TKey, TElement>(this IEnumerable<TSource> rpSource, Func<TSource, TKey> rpKeySelector, Func<TSource, TElement> rpElementSelector, IComparer<TKey> rpComparer)
+        {
+            if (rpSource == null)
+                throw new ArgumentNullException(nameof(rpSource));
+            if (rpKeySelector == null)
+                throw new ArgumentNullException(nameof(rpKeySelector));
+            if (rpElementSelector == null)
+                throw new ArgumentNullException(nameof(rpElementSelector));
+
+            var rResult = new SortedList<TKey, TElement>(rpComparer);
             foreach (var rItem in rpSource)
                 rResult.Add(rpKeySelector(rItem), rpElementSelector(rItem));
 
