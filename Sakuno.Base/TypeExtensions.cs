6b6337541d5dac197f3d0babd65cd4a2e1940475
神樹桜乃 <kodama@sakuno.moe>
2017-03-14 01:44:32 +0800

    Some extension methods

---
+++ Sakuno.Base/TypeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Sakuno
+{
+    public static class TypeExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsAssignableFrom<T>(this Type rpType) => rpType.IsAssignableFrom(typeof(T));
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsSubclassOf<T>(this Type rpType) => rpType.IsSubclassOf(typeof(T));
+    }
+}
