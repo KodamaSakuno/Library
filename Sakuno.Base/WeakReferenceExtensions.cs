eb9f3d4d4c65897ee75981bea4604430091e100e
神樹桜乃 <kodama@sakuno.moe>
2017-03-15 16:46:24 +0800

    A extension method for weak reference

---
+++ Sakuno.Base/WeakReferenceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sakuno
+{
+    public static class WeakReferenceExtensions
+    {
+        public static T GetTargetOrDefault<T>(this WeakReference<T> rpWeakReference) where T : class
+        {
+            T rResult;
+            rpWeakReference.TryGetTarget(out rResult);
+
+            return rResult;
+        }
+    }
+}
